5fdbf1e89b18ed4b799f14f1c2f228a9ddf8d557
Susana Reyes <60724497+susi1603@users.noreply.github.com>
2021-06-29 19:35:06 -0500

    Inline Styling in JSX

--- src/index.js
+++ src/index.js
@@ -7,11 +7,20 @@ const myLuckyNumber = Math.floor(Math.random() * 10);
 const firstName = "Susy";
 const lastName = "Reyes";
 
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
+
 ReactDOM.render(
   <div>
     <h1>Hello World! I am {`${firstName} ${lastName}`}</h1>
     <p>And this is your lucky number {myLuckyNumber} </p>
-    <p></p>
+    <p style={{ color: "purple" }}>Color purple inline-style.</p>
+    <h1 style={customStyle}> A custom style without CSS</h1>
   </div>,
   document.getElementById("root")
 );
